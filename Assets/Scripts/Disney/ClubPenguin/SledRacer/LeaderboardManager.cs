6c3d813a98729b390530123c9a87c7f684854fac
Wednesday Addams <21669120+wednesday2024@users.noreply.github.com>
2026-04-05 05:41:46 -0500

    Update LeaderboardManager.cs

--- Assets/Scripts/Disney/ClubPenguin/SledRacer/LeaderboardManager.cs
+++ Assets/Scripts/Disney/ClubPenguin/SledRacer/LeaderboardManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using DevonLocalization.Core;
 using Disney.ClubPenguin.Service.MWS;
 using Disney.ClubPenguin.Service.MWS.Domain;
@@ -6,152 +7,223 @@ using UnityEngine;
 
 namespace Disney.ClubPenguin.SledRacer
 {
-	public class LeaderboardManager
-	{
-		private const string WEEKLY_LEADERBOARD_DURATION = "weekly";
+    public class LeaderboardManager
+    {
+        private const string WEEKLY_LEADERBOARD_DURATION = "weekly";
 
-		private const int WEEKLY_LEADERBOARD_HISTORIC_PERIOD = 1;
+        private const int WEEKLY_LEADERBOARD_HISTORIC_PERIOD = 1;
 
-		private IMWSClient mwsClient;
+        private IMWSClient mwsClient;
 
-		private LeaderBoardResponse cachedFriendsResponse;
+        private LeaderBoardResponse cachedFriendsResponse;
 
-		public LeaderboardManager()
-		{
-			mwsClient = Service.Get<IMWSClient>();
-		}
+        public LeaderboardManager()
+        {
+            mwsClient = Service.Get<IMWSClient>();
+        }
 
-		public void startGame(string gameType, Action<Game> callback)
-		{
-			mwsClient.StartGame(gameType, delegate(IStartGameResponse response)
-			{
-				callback(response.Game);
-			});
-		}
+        public void startGame(string gameType, Action<Game> callback)
+        {
+            if (!IsPlayerLoggedIn())
+            {
+                callback(null);
+                return;
+            }
 
-		public void saveGame(GameResult gameResult, Action<GameResult> callback)
-		{
-			mwsClient.SetGameResult(gameResult, delegate(ISetGameResultResponse response)
-			{
-				handleSaveGameResponse(callback, response.GameResult);
-			});
-		}
+            mwsClient.StartGame(gameType, delegate (IStartGameResponse response)
+            {
+                callback(response.Game);
+            });
+        }
 
-		private void handleSaveGameResponse(Action<GameResult> callback, GameResult result)
-		{
-			if (result != null)
-			{
-				long playerId = result.playerId;
-				foreach (PlayerResult playerResult in result.playerResults)
-				{
-					if (playerResult.PlayerId == playerId)
-					{
-						injectIntoCachedFriendsData(result.playerId, playerResult.Result);
-					}
-				}
-			}
-			callback(result);
-		}
+        public void saveGame(GameResult gameResult, Action<GameResult> callback)
+        {
+            if (!IsPlayerLoggedIn())
+            {
+                callback(null);
+                return;
+            }
 
-		private void injectIntoCachedFriendsData(long playerId, string scoreStr)
-		{
-			int result = 0;
-			int.TryParse(scoreStr, out result);
-			if (cachedFriendsResponse == null || result == 0)
-			{
-				return;
-			}
-			int rank = 1;
-			foreach (LeaderBoardHighScore player in cachedFriendsResponse.Players)
-			{
-				if (result < player.Score)
-				{
-					rank = player.Rank + 1;
-				}
-				else if (result == player.Score)
-				{
-					rank = player.Rank;
-				}
-				if (player.PlayerId == playerId)
-				{
-					if (player.Score < result)
-					{
-						player.Score = result;
-						player.Rank = rank;
-						SortLeaderboard(cachedFriendsResponse, null);
-					}
-					break;
-				}
-			}
-		}
+            mwsClient.SetGameResult(gameResult, delegate (ISetGameResultResponse response)
+            {
+                handleSaveGameResponse(callback, response.GameResult);
+            });
+        }
 
-		public void LoadMyAllTimeHighScore(string gameType, Action<int> SetHighScoreCallback)
-		{
-			mwsClient.GetMyHighScore(gameType, delegate(IMyHighScoreResponse score)
-			{
-				SetHighScoreCallback(score.Score);
-			});
-		}
+        private void handleSaveGameResponse(Action<GameResult> callback, GameResult result)
+        {
+            if (result != null)
+            {
+                long playerId = result.playerId;
+                foreach (PlayerResult playerResult in result.playerResults)
+                {
+                    if (playerResult.PlayerId == playerId)
+                    {
+                        injectIntoCachedFriendsData(result.playerId, playerResult.Result);
+                    }
+                }
+            }
+            callback(result);
+        }
 
-		public virtual void LoadAllHighScores(string gameType, Action<LeaderBoardResponse> SetHighScoresCallback)
-		{
-			mwsClient.GetLeaderBoard(gameType, currentLanguage(), "weekly", false, delegate(IGetLeaderBoardResponse response)
-			{
-				SortLeaderboard(response.LeaderBoard, SetHighScoresCallback);
-			});
-		}
+        private void injectIntoCachedFriendsData(long playerId, string scoreStr)
+        {
+            int result = 0;
+            int.TryParse(scoreStr, out result);
+            if (cachedFriendsResponse == null || result == 0)
+            {
+                return;
+            }
+            int rank = 1;
+            foreach (LeaderBoardHighScore player in cachedFriendsResponse.Players)
+            {
+                if (result < player.Score)
+                {
+                    rank = player.Rank + 1;
+                }
+                else if (result == player.Score)
+                {
+                    rank = player.Rank;
+                }
+                if (player.PlayerId == playerId)
+                {
+                    if (player.Score < result)
+                    {
+                        player.Score = result;
+                        player.Rank = rank;
+                        SortLeaderboard(cachedFriendsResponse, null);
+                    }
+                    break;
+                }
+            }
+        }
 
-		public void LoadFriendHighScores(string gameType, Action<LeaderBoardResponse> SetHighScoresCallback)
-		{
-			mwsClient.GetLeaderBoard(gameType, currentLanguage(), "weekly", true, delegate(IGetLeaderBoardResponse response)
-			{
-				cachedFriendsResponse = response.LeaderBoard;
-				SortLeaderboard(response.LeaderBoard, SetHighScoresCallback);
-			});
-		}
+        public void LoadMyAllTimeHighScore(string gameType, Action<int> SetHighScoreCallback)
+        {
+            if (!IsPlayerLoggedIn())
+            {
+                SetHighScoreCallback(HighScore.GetOfflineHighScoreFromPrefs());
+                return;
+            }
 
-		public bool LoadCachedFriendHighScores(string gameType, Action<LeaderBoardResponse> SetHighScoresCallback)
-		{
-			if (cachedFriendsResponse != null)
-			{
-				SetHighScoresCallback(cachedFriendsResponse);
-				return true;
-			}
-			LoadFriendHighScores(gameType, SetHighScoresCallback);
-			return false;
-		}
+            mwsClient.GetMyHighScore(gameType, delegate (IMyHighScoreResponse score)
+            {
+                SetHighScoreCallback(score.Score);
+            });
+        }
 
-		public bool AreFriendHighScoresCached()
-		{
-			return cachedFriendsResponse != null;
-		}
+        public virtual void LoadAllHighScores(string gameType, Action<LeaderBoardResponse> SetHighScoresCallback)
+        {
+            if (!IsPlayerLoggedIn())
+            {
+                SetHighScoresCallback(BuildOfflineLeaderboard());
+                return;
+            }
 
-		public void ClearCachedFriendHighScores()
-		{
-			Debug.Log("ClearCachedFriendHighScores");
-			cachedFriendsResponse = null;
-		}
+            mwsClient.GetLeaderBoard(gameType, currentLanguage(), "weekly", false, delegate (IGetLeaderBoardResponse response)
+            {
+                SortLeaderboard(response.LeaderBoard, SetHighScoresCallback);
+            });
+        }
 
-		public void GetRewardStatus(string gameType, Action<LeaderBoardRewardStatus> GetRewardStatusCallback)
-		{
-			mwsClient.GetLeaderBoardRewardStatus(gameType, "weekly", 1, delegate(IGetLeaderBoardRewardStatus response)
-			{
-				GetRewardStatusCallback(response.RewardStatus);
-			});
-		}
+        public void LoadFriendHighScores(string gameType, Action<LeaderBoardResponse> SetHighScoresCallback)
+        {
+            if (!IsPlayerLoggedIn())
+            {
+                LeaderBoardResponse offline = BuildOfflineLeaderboard();
+                cachedFriendsResponse = offline;
+                SetHighScoresCallback(offline);
+                return;
+            }
 
-		private void SortLeaderboard(LeaderBoardResponse leaderboard, Action<LeaderBoardResponse> SetHighScoresCallback)
-		{
-			leaderboard.Players.Sort((LeaderBoardHighScore x, LeaderBoardHighScore y) => (x.Rank == y.Rank) ? (-1 * x.Score.CompareTo(y.Score)) : x.Rank.CompareTo(y.Rank));
-			if (SetHighScoresCallback != null)
-			{
-				SetHighScoresCallback(leaderboard);
-			}
-		}
+            mwsClient.GetLeaderBoard(gameType, currentLanguage(), "weekly", true, delegate (IGetLeaderBoardResponse response)
+            {
+                cachedFriendsResponse = response.LeaderBoard;
+                SortLeaderboard(response.LeaderBoard, SetHighScoresCallback);
+            });
+        }
 
-		private string currentLanguage()
-		{
-			return LocalizationLanguage.GetLanguageString(Localizer.Instance.Language);
-		}
-	}
+        public bool LoadCachedFriendHighScores(string gameType, Action<LeaderBoardResponse> SetHighScoresCallback)
+        {
+            if (cachedFriendsResponse != null)
+            {
+                SetHighScoresCallback(cachedFriendsResponse);
+                return true;
+            }
+            LoadFriendHighScores(gameType, SetHighScoresCallback);
+            return false;
+        }
+
+        public bool AreFriendHighScoresCached()
+        {
+            return cachedFriendsResponse != null;
+        }
+
+        public void ClearCachedFriendHighScores()
+        {
+            Debug.Log("ClearCachedFriendHighScores");
+            cachedFriendsResponse = null;
+        }
+
+        public void GetRewardStatus(string gameType, Action<LeaderBoardRewardStatus> GetRewardStatusCallback)
+        {
+            if (!IsPlayerLoggedIn())
+            {
+                GetRewardStatusCallback(new LeaderBoardRewardStatus());
+                return;
+            }
+
+            mwsClient.GetLeaderBoardRewardStatus(gameType, "weekly", 1, delegate (IGetLeaderBoardRewardStatus response)
+            {
+                GetRewardStatusCallback(response.RewardStatus);
+            });
+        }
+
+        private LeaderBoardResponse BuildOfflineLeaderboard()
+        {
+            PlayerDataService pds = Service.Get<PlayerDataService>();
+            PlayerData playerData = pds.PlayerData;
+
+            int score = HighScore.GetOfflineHighScoreFromPrefs();
+
+            LeaderBoardHighScore entry = new LeaderBoardHighScore();
+            entry.Rank = 1;
+            entry.Name = playerData.Account.Username;
+            entry.PlayerSWID = playerData.Account.PlayerSwid;
+            entry.Score = score;
+            entry.Colour = playerData.Account.Colour;
+
+            LeaderBoardResponse response = new LeaderBoardResponse();
+            response.Players = new List<LeaderBoardHighScore> { entry };
+            response.Countdown = 0;
+
+            return response;
+        }
+
+        private bool IsPlayerLoggedIn()
+        {
+            try
+            {
+                return Service.Get<PlayerDataService>().IsPlayerLoggedIn();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void SortLeaderboard(LeaderBoardResponse leaderboard, Action<LeaderBoardResponse> SetHighScoresCallback)
+        {
+            leaderboard.Players.Sort((LeaderBoardHighScore x, LeaderBoardHighScore y) => (x.Rank == y.Rank) ? (-1 * x.Score.CompareTo(y.Score)) : x.Rank.CompareTo(y.Rank));
+            if (SetHighScoresCallback != null)
+            {
+                SetHighScoresCallback(leaderboard);
+            }
+        }
+
+        private string currentLanguage()
+        {
+            return LocalizationLanguage.GetLanguageString(Localizer.Instance.Language);
+        }
+    }
 }
