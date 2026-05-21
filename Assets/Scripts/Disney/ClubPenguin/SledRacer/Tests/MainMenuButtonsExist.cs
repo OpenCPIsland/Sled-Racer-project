7388bef23ca188184ab895a2351642e1f36ee526
Eleven <21669120+wednesday2024@users.noreply.github.com>
2026-05-20 20:28:39 -0500

    Update MainMenuButtonsExist.cs

--- Assets/Scripts/Disney/ClubPenguin/SledRacer/Tests/MainMenuButtonsExist.cs
+++ Assets/Scripts/Disney/ClubPenguin/SledRacer/Tests/MainMenuButtonsExist.cs
@@ -6,44 +6,43 @@ using UnityEngine.UI;
 
 namespace Disney.ClubPenguin.SledRacer.Tests
 {
-	public class MainMenuButtonsExist : UIIntegrationTest
-	{
-		public string[] ExpectedButtonNames;
+    public class MainMenuButtonsExist : UIIntegrationTest
+    {
+        public string[] ExpectedButtonNames;
 
-		public string[] ExcludedButtonContainerNames;
+        public string[] ExcludedButtonContainerNames;
 
-		public IList<GameObject> ExcludedButtonContainers = new List<GameObject>();
+        public IList<GameObject> ExcludedButtonContainers = new List<GameObject>();
 
-		protected override void RunTest()
-		{
-			string[] excludedButtonContainerNames = ExcludedButtonContainerNames;
-			foreach (string name in excludedButtonContainerNames)
-			{
-				ExcludedButtonContainers.Add(GameObject.Find(name));
-			}
-			Object[] array = Object.FindObjectsOfType(typeof(Button));
-			foreach (Object @object in array)
-			{
-				Button button = (Button)@object;
-				UnityEngine.Debug.Log("Found Button: " + button.GetPath());
-				if (button.gameObject.transform.IsChildOf(panel.transform))
-				{
-					bool flag = true;
-					foreach (GameObject excludedButtonContainer in ExcludedButtonContainers)
-					{
-						if (button.gameObject.transform.IsChildOf(excludedButtonContainer.transform))
-						{
-							UnityEngine.Debug.Log("Skipping button: " + button.name + " in the container: " + excludedButtonContainer.name);
-							flag = false;
-							break;
-						}
-					}
-					if (flag)
-					{
-						IntegrationTest.Assert(ExpectedButtonNames.Contains(button.name));
-					}
-				}
-			}
-		}
-	}
+        protected override void RunTest()
+        {
+            string[] excludedButtonContainerNames = ExcludedButtonContainerNames;
+            foreach (string name in excludedButtonContainerNames)
+            {
+                ExcludedButtonContainers.Add(GameObject.Find(name));
+            }
+            Button[] array = Object.FindObjectsByType<Button>(FindObjectsSortMode.None);
+            foreach (Button button in array)
+            {
+                UnityEngine.Debug.Log("Found Button: " + button.GetPath());
+                if (button.gameObject.transform.IsChildOf(panel.transform))
+                {
+                    bool flag = true;
+                    foreach (GameObject excludedButtonContainer in ExcludedButtonContainers)
+                    {
+                        if (button.gameObject.transform.IsChildOf(excludedButtonContainer.transform))
+                        {
+                            UnityEngine.Debug.Log("Skipping button: " + button.name + " in the container: " + excludedButtonContainer.name);
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (flag)
+                    {
+                        IntegrationTest.Assert(ExpectedButtonNames.Contains(button.name));
+                    }
+                }
+            }
+        }
+    }
 }
